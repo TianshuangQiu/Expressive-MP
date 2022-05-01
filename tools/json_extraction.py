e9f6560ff70d6ad839001673b20fc7670be69d4c
TianshuangQiu <ethantqiu@berkeley.edu>
2022-05-01 12:47:43 -0700

    completed pipeline

--- tools/json_extraction.py
+++ tools/json_extraction.py
@@ -17,7 +17,7 @@ def readfile(n):
     else:
         numstring = str(n)
 
-    filename = "/home/akita/autolab/Expressive-MP/waypoints/IMG_4015/IMG_4015_00000000" + \
+    filename = "/home/akita/autolab/Expressive-MP/waypoints/IMG_4010/IMG_4010_00000000" + \
         numstring + "_keypoints.json"
 
     item = pd.read_json(filename)
@@ -86,7 +86,7 @@ def num_deriv(array, t):
 
 theta_list = []
 
-for n in range(2116):
+for n in range(1522):
     d = readfile(n)["people"][0]
     series = d['pose_keypoints_2d']
     pt0 = series[0:2]
@@ -119,5 +119,5 @@ wr = data[1] - data[2]
 
 data_stack = np.vstack([sh, el, wr])
 
-with open("saves/IMG_4015", "wb") as f:
+with open("saves/IMG_4010", "wb") as f:
     np.save(f, data_stack)
--- tools/traj_generation.py
+++ tools/traj_generation.py
@@ -1,13 +1,14 @@
-from cProfile import label
+import math
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
-from scipy.misc import derivative
+import scipy.optimize
+
 
 TIMESTEP = 0.001  # needs to evenly divide 0.04, should match input to t_toss when called
 FRAMERATE = 25  # FPS in original video, should be 25
 prev_hand_kp = [0, 0]
-with open("saves/IMG_4015", "rb") as f:
+with open("saves/IMG_4010", "rb") as f:
     stack = np.load(f)
 
 sh = stack[0]
@@ -38,6 +39,30 @@ def num_deriv(array, t):
         grad = np.gradient(a, t, axis=0)
         stack = (grad if stack is None else np.vstack([stack, grad]))
     return stack.T
+
+
+def fit_sin(tt, yy):
+    '''Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"'''
+    tt = np.array(tt)
+    yy = np.array(yy)
+    ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+    Fyy = abs(np.fft.fft(yy))
+    # excluding the zero frequency "peak", which is related to offset
+    guess_freq = abs(ff[np.argmax(Fyy[1:])+1])
+    guess_amp = np.std(yy) * 2.**0.5
+    guess_offset = np.mean(yy)
+    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
+
+    def sinfunc(t, A, w, p, c): return A * np.sin(w*t + p) + c
+    try:
+        popt, pcov = scipy.optimize.curve_fit(
+            sinfunc, tt, yy, p0=guess, maxfev=10000)
+    except:
+        return {"fitfunc": lambda x: 0}
+    A, w, p, c = popt
+    f = w/(2.*np.pi)
+    def fitfunc(t): return A * np.sin(w*t + p) + c
+    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc}
 
 
 def do_fft():
@@ -95,14 +120,14 @@ def do_cc():
     sh_zeros = filter_zeros(sh_d)
     el_zeros = filter_zeros(el_d)
     wr_zeros = filter_zeros(wr_d)
-    split = np.array(np.split(sh, sh_zeros) +
-                     np.split(el, el_zeros) +
-                     np.split(wr, wr_zeros),
-                     dtype=object)
+    horizontal = np.split(sh, sh_zeros) + \
+        np.split(el, el_zeros) + \
+        np.split(wr, wr_zeros)
+    split = np.array(horizontal, dtype=object)
 
     def cross(arr1, arr2):
         """
-        Cross correlation in case the two vectors do not 
+        Cross correlation in case the two vectors do not
         have the same length
         """
         length = np.max([len(arr1), len(arr2)])
@@ -113,25 +138,64 @@ def do_cc():
         arr2 = arr2 / norm2
         arr1 = np.pad(arr1, length)
         arr2 = np.pad(arr2, length)
-        return np.max(np.correlate(arr1, arr2, mode='full'))
+        return np.max(np.correlate(arr1, arr2, mode='full')) * math.sqrt(norm1 * norm2)
 
     cc = np.zeros((split.shape[0], split.shape[0]))
     for i in range(len(split)):
         for j in range(len(split)):
             val = cross(split[i], split[j])
-            cc[i][j] = 0 if val == 1 else val
+            cc[i][j] = 0 if i == j else val
+    cc = np.max(cc, axis=0)
+    sort = np.argsort(cc)
 
-    cc = np.max(cc, axis=1)
-    print(cc)
-    fig, ax = plt.subplots()
-    for s in split:
-        plt.plot(s)
+    for i in range(1, 5):
+        curr_dat = horizontal[sort[i]]
+        x_dat = np.arange(len(curr_dat))
+        plt.plot(curr_dat, label=f"{i}th principle component")
+        rst = fit_sin(x_dat, curr_dat)["fitfunc"]
+        plt.plot(np.array(list(map(rst, x_dat))), label=f"{i}th fit")
     # plt.scatter(sh_zeros, sh[sh_zeros], c='r', label="split points")
     plt.title("Extracted Vectors")
-    plt.xlabel("Time (seconds)")
+    plt.xlabel("Time (steps)")
     plt.ylabel("Angle (rad)")
     ax = plt.legend()
     plt.show()
+
+    principle_motion = np.array(t)
+    for i in range(1, 20):
+        curr_dat = horizontal[sort[i]]
+        x_dat = np.arange(len(curr_dat))
+        rst = fit_sin(x_dat, horizontal[sort[i]])["fitfunc"]
+        principle_motion = np.vstack([principle_motion, list(map(rst, t))])
+
+    principle_motion = principle_motion[1:]
+
+    rand_idx = np.random.randint(19, size=9)
+    first_joint = principle_motion[rand_idx[0]] + \
+        principle_motion[rand_idx[1]] + \
+        principle_motion[rand_idx[2]]
+    first_joint = 500/np.linalg.norm(first_joint) * first_joint
+    position = np.vstack([first_joint,
+                          sh,
+                          el,
+                          wr,
+                          principle_motion[rand_idx[3]] +
+                          principle_motion[rand_idx[4]] +
+                          principle_motion[rand_idx[5]],
+                          principle_motion[rand_idx[7]] +
+                          principle_motion[rand_idx[8]],
+                          ]).T
+
+    position = linear_interp(position, int(1 / FRAMERATE / TIMESTEP))
+    t_int = linear_interp(t[np.newaxis].T, int(1 / FRAMERATE / TIMESTEP))
+
+    velocity = num_deriv(position, TIMESTEP)
+    acceleration = num_deriv(velocity, TIMESTEP)
+    jerk = num_deriv(acceleration, TIMESTEP)
+
+    output = np.hstack([t_int, position, velocity, acceleration, jerk])
+    output = np.round_(output, decimals=5)
+    np.savetxt("IMG_4015.dat", output, fmt="%10.5f", delimiter='\t')
 
 
 # Rest of program
